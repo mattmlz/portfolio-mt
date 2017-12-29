47e2bf9b03c8c3c5bf1fa23c3594ec06531fae25
Unknown <matthieu.toussaint@hetic.net>
2017-12-29 16:48:17 +0100

    redirection to homepage with click on logo

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+//Set variables
+const $logo = document.querySelector('.logoHeader')
+
+
+/********** 
+* GENERAL * 
+**********/
+//back to homepage with click on header logo
+$logo.addEventListener('click', () => {
+  document.location.href='../index.html'
+})
