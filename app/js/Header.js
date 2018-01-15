f808e6e47e2c56ab485a7f9d34710eecf413bec2
Unknown <matthieu.toussaint@hetic.net>
2018-01-15 15:23:12 +0100

    change js to POO format

---
+++ app/js/Header.js
@@ -0,0 +1,15 @@
+class Header {
+  constructor() {
+    this.links()
+  }
+
+  links() {
+    //Set variables
+    const $logo = document.querySelector('.logoHeader')
+
+    //back to homepage with click on header logo
+    $logo.addEventListener('click', () => {
+      document.location.href='../index.html'
+    })
+  }
+}
--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,1 @@
-//Set variables
-const $logo = document.querySelector('.logoHeader')
-
-
-/* GENERAL */
-//back to homepage with click on header logo
-$logo.addEventListener('click', () => {
-  document.location.href='../index.html'
-})
+const header = new Header()
