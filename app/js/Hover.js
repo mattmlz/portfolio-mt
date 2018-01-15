4a053195662d22af8437acca92411ae95626ea21
Unknown <matthieu.toussaint@hetic.net>
2018-01-15 16:45:35 +0100

    add link to dev works & design works

--- app/js/Hover.js
+++ app/js/Hover.js
@@ -8,6 +8,7 @@ class Hover {
     const $dev = $sections.querySelector('.sectionDev')
     const $design = $sections.querySelector('.sectionDesign')
     
+    //Actions on $dev
     $dev.addEventListener('mouseover', () => {
       $dev.style.opacity = '1'
       $dev.style.cursor = 'pointer'
@@ -16,12 +17,20 @@ class Hover {
       $dev.style.opacity = '0.5'
     })
 
+    $dev.addEventListener('click', () => {
+      window.location.href = '../pages/work/dev.html'
+    })
+
+    //Actions on $design
     $design.addEventListener('mouseover', () => {
       $design.style.opacity = '1'
       $design.style.cursor = 'pointer'
     })
     $design.addEventListener('mouseleave', () => {
       $design.style.opacity = '0.5'
+    })
+    $design.addEventListener('click', () => {
+      window.location.href = '../pages/work/design.html'
     })
   }
 }
