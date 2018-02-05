d3193f82b8c9105e0a6c48cd12ed15501aa0fa55
mattmlz <matthieu.toussaint@hetic.net>
2018-02-05 14:33:13 +0100

    :sparkles: PAGES>WORK>DEV>Add loader while page loading
    PAGES>WORK>DEV>Add loader while page loading #1

--- app/js/LazyLoadingDev.js
+++ app/js/LazyLoadingDev.js
@@ -4,15 +4,23 @@ class LazyLoadingDev {
   }
 
   lazyLoadDev() {
+    //Set variables
+    const $loader = document.querySelector('.breeding-rhombus-spinner')
     //Put nodeList in a 'real' array
     const $lazyLoad = [].slice.call(document.querySelectorAll('.lazy-load'))
     
+    //Add loader until page is loading
+    document.addEventListener("DOMContentLoaded", function(event) {
+      $loader.style.display = 'inline'
+    })
     //Wait the load of the page to show the divs
     window.addEventListener('load', (event) => {
       for(let i = 0; i < $lazyLoad.length; i++) {
         const $div = $lazyLoad[i]
         $div.classList.add('loaded')
       }
+      //Remove loader
+      $loader.style.display = 'none'
     })
   }
 }
