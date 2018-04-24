d67fdb69be94d6a34c5e4558232cb2be14789372
mattmlz <matthieu.toussaint@hetic.net>
2018-04-24 16:39:19 +0200

    :art: starting portfolio v2.0
    starting portfolio v2.0 #1

---
+++ views/includes/footer.php
@@ -0,0 +1,19 @@
+</main>
+<footer class="page-footer">
+    <div class="container center">© 2018 MATTHIEU TOUSSAINT</div>
+</footer>
+
+  <script src="assets/js/app.js"></script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-90047916-3"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'UA-90047916-3');
+  </script>
+  <!-- Compiled and minified JavaScript of Materialize -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+</body>
+</html>
---
+++ views/includes/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/pictures/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/pictures/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/pictures/favicons/favicon-16x16.png">
+  <link rel="manifest" href="/assets/pictures/favicons/manifest.json">
+  <link rel="mask-icon" href="/assets/pictures/favicons/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="theme-color" content="#ffffff">
+  <!-- Compiled and minified CSS of Materialize-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- Styles -->
+  <link rel="stylesheet" href="/assets/styles/index.css">
+  <title>Matthieu TOUSSAINT</title>
+</head>
+
+<body>
+<nav>
+  <div class="nav-wrapper">
+    <a href="/" class="brand-logo center">Matthieu TOUSSAINT - Developer</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="/works">Works</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contact"><i class="material-icons left">email</i></a></li>
+    </ul>
+  </div>
+</nav>
+
+<main>
