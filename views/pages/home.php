2bdc2dff3e29a975b8af479a12a89a88e3c8d19d
mattmlz <matthieu.toussaint@hetic.net>
2018-04-24 16:44:41 +0200

    :pushpin: fix footer to bottom of page
    fix footer to bottom of page #1

--- views/pages/home.php
+++ views/pages/home.php
@@ -2,7 +2,13 @@
   require_once 'views/includes/header.php';
 ?>
 
-
+<div class="container home-informations">
+  <div class="row">
+    <div class="col s6"></div>
+    <div class="col s6">Matthieu TOUSSAINT</div>
+  </div>
+  <div class="row">Web student @ HETIC</div>
+</div>
 
 <?php 
   require_once 'views/includes/footer.php';
